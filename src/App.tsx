aff264706e9caea6713d5df0cb71dd8389498cea
yasuke <yassine.ksabi.dev.blockchain@gmail.com>
2023-02-28 23:35:16 +0100

    * Split code into multiple components * fix #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {ethers, Event} from "ethers";
 import {ERC20Model, WalletModel} from "./models";
-import {CHAIN_POLYGON, CHAIN_GOERLI, ChainList} from "./constants";
+import {CHAIN_GOERLI, CHAIN_POLYGON, ChainList} from "./constants";
 import {toast} from "react-toastify";
-import ABI from "./artifacts/ABI.json"
+import DataTable, {TableColumn} from "react-data-table-component";
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const connectAutomtically = urlParams.get('connectAutomatically')
 
+const rpcProvider = new ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${process.env.REACT_APP_INFURA_API_KEY}`)
+const web3Provider = new ethers.providers.Web3Provider(window.ethereum)
+
 function App() {
-    const web3Provider = new ethers.providers.Web3Provider(window.ethereum)
-    const rpcProvider = new ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${process.env.REACT_APP_INFURA_API_KEY}`)
-
     const [connectedWallet, setConnectedWallet] = useState<WalletModel | null>(null)
-    const [erc20, setErc20] = useState<ERC20Model | null>(null)
-
-    const [loadingErc20data, setLoadingErc20data] = useState<boolean>(true)
 
     useEffect(() => {
         if (connectAutomtically) {
@@ -28,7 +25,6 @@ function App() {
             }
         }
     }, [])
-
 
     const handleWalletLogin = () => {
         toast.info("Connecting ...", {autoClose: false})
@@ -49,18 +45,15 @@ function App() {
 
                         // Get account address
                         web3Provider.getSigner().getAddress().then((address: string) => {
+
                             setConnectedWallet(prev => ({...prev, address}))
                             // Set account balance
                             web3Provider.getBalance(address).then(balance => {
-
 
                                 setConnectedWallet(prev => ({...prev, balanceInWei: balance.toString()}))
 
                                 toast.dismiss()
                                 toast.success("Connnected!")
-
-                                // Get ERC20 Data
-                                getERC20Data(address)
 
                                 // Listen to all account changes & network swtich
                                 listeners()
@@ -93,63 +86,6 @@ function App() {
         } else {
             toast.error("No wallet detected")
         }
-    }
-
-    // Name, symbol, totalSupply, balanceOf signer, tokens transfers from signer, tokens transfers to signer (https://docs.ethers.org/v5/api/contract/example/#erc20-queryfilter)
-    const getERC20Data = (signerAddress: string) => {
-        setLoadingErc20data(true)
-
-        // @ts-ignore
-        const contract = new ethers.Contract(process.env.REACT_APP_GOERLI_ERC20_TOKEN_ADDRESS, [
-            "function name() view returns (string)",
-            "function symbol() view returns (string)",
-            "function totalSupply() view returns (uint256)",
-            "function balanceOf(address) view returns (uint)",
-            "event Transfer(address indexed from, address indexed to, uint value)"
-        ], rpcProvider)
-
-
-        // Name
-        contract.name().then((name: string) => {
-
-            setErc20(prev => ({...prev, name}))
-            // Symbol
-            contract.symbol().then((symbol: string) => {
-                setErc20(prev => ({...prev, symbol: symbol}))
-                // Total supplu
-                contract.totalSupply().then((supply: any) => {
-                    setErc20(prev => ({...prev, totalSupply: ethers.utils.formatEther(supply)}))
-
-                    // Balance of signer
-                    contract.balanceOf(signerAddress).then((balance: any) => {
-
-                        setErc20(prev => ({...prev, balanceOfConnectedAccount: ethers.utils.formatEther(balance)}))
-
-                        // Transfers from signer
-                        const filterTransferFrom = contract.filters.Transfer(signerAddress, null) // Tokens sent from signer
-                        const filterTransferTo = contract.filters.Transfer(null, signerAddress) // Tokens sent to signer
-                        contract.queryFilter(filterTransferFrom).then((r: Event[]) => {
-
-                            setErc20(prev => ({...prev, events: {...prev?.events, transferFrom: r}}))
-                            contract.queryFilter(filterTransferTo).then((r: Event[]) => {
-                                setErc20(prev => ({...prev, events: {...prev?.events, transferTo: r}}))
-
-                                // Listen to incoming events from signer:
-                                contract.on(filterTransferFrom, (from, to, amount, event) => {
-                                    setErc20(prev => ({...prev, events: {...prev?.events, transferFrom: prev?.events?.transferFrom ? [...prev.events.transferFrom, event]: []}}))
-                                });
-                                // Listen to incoming events to signer:
-                                contract.on(filterTransferTo, (from, to, amount, event) => {
-                                    setErc20(prev => ({...prev, events: {...prev?.events, transferTo: prev?.events?.transferTo ? [...prev.events.transferTo, event]: []}}))
-                                });
-
-                                setLoadingErc20data(false)
-                            })
-                        })
-                    })
-                })
-            })
-        })
     }
 
     const handleWalletLogout = () => {
@@ -229,21 +165,6 @@ function App() {
         })
     }
 
-    const handleAddGoerliErc20 = async () => {
-        await window.ethereum.request({
-            method: 'wallet_watchAsset',
-            params: {
-                type: 'ERC20',
-                options: {
-                    address: process.env.REACT_APP_GOERLI_ERC20_TOKEN_ADDRESS,
-                    symbol: process.env.REACT_APP_GOERLI_ERC20_TOKEN_SYMBOL,
-                    decimals: 18,
-                    image: ''
-                }
-            }
-        })
-    }
-
     const listeners = () => {
         // Listeners
         // -- Everytime an accountsChanged event fires, it will reload the page ( the best possible way to manage chain changes )
@@ -309,93 +230,163 @@ function App() {
 
                     <hr/>
 
-                    {connectedWallet.chain?.id == CHAIN_GOERLI.id && <>
-                        {loadingErc20data && <span>Loading ERC20 data & events...</span>}
-
-                        {!loadingErc20data && erc20 && <div className={"container mt-3"}>
-                            <div className="card">
-                                <div className="card-header">
-                                    <div className="text-center">
-                                        ERC20 {erc20.name}
-                                    </div>
-
-                                </div>
-                                <div className="card-body">
-                                    <div className="text-center mb-4">
-                                        <button className={"btn btn-success"}
-                                                onClick={() => handleAddGoerliErc20()}>
-                                            Add {erc20.name} to your Wallet
-                                        </button>
-                                    </div>
-                                    <ul>
-                                        <li>What's the name of the token? {erc20.name}</li>
-                                        <li>How many {erc20.symbol} are they circulating (total supply)? {erc20.totalSupply}</li>
-                                        <li>How many {erc20.symbol} tokens does the connected account have? {erc20.balanceOfConnectedAccount}</li>
-                                    </ul>
-
-                                    {/*Transfer from events*/}
-                                    <div className={"pt-2"}>
-                                        <h5>Transfers From signer</h5>
-                                        {erc20.events!.transferFrom!.length > 0 ?
-                                            <>
-                                                <div className={"row pt-3"}>
-                                                    <h6 className={"col-4"}>#</h6>
-                                                    <h6 className={"col-2"}>Block (position)</h6>
-                                                    <h6 className={"col-4"}>To</h6>
-                                                    <h6 className={"col-2"}>Value in ETH</h6>
-                                                </div>
-                                                <hr/>
-                                                {
-                                                    erc20.events?.transferFrom?.map((item, key) => (
-                                                        <div className={"row mb-2"} key={key}>
-                                                            <div className={"col-4"}>{item.transactionHash}</div>
-                                                            <div className={"col-2"}>{item.blockNumber} ({item.transactionIndex}th)</div>
-                                                            <div className={"col-4"}>{item.args?.to}</div>
-                                                            <div className={"col-2"}>{ethers.utils.formatEther(item.args?.value)}</div>
-                                                        </div>
-                                                    ))
-                                                }
-                                            </> : <div className={"text-info"}>No Transfers from {connectedWallet.address}</div>
-                                        }
-                                    </div>
-
-
-                                    {/*Transfer to events*/}
-                                    <div className={"pt-3"}>
-                                        <h5>Transfers To Signer</h5>
-                                        {erc20.events!.transferTo!.length > 0 ?
-                                            <>
-                                                <div className={"row pt-3"}>
-                                                    <h6 className={"col-4"}>#</h6>
-                                                    <h6 className={"col-2"}>Block (position)</h6>
-                                                    <h6 className={"col-4"}>To</h6>
-                                                    <h6 className={"col-2"}>Value in ETH</h6>
-                                                </div>
-                                                <hr/>
-                                                {
-                                                    erc20.events?.transferTo?.map((item, key) => (
-                                                        <div className={"row mb-2"} key={key}>
-                                                            <div className={"col-4"}>{item.transactionHash}</div>
-                                                            <div className={"col-2"}>{item.blockNumber} ({item.transactionIndex}th)</div>
-                                                            <div className={"col-4"}>{item.args?.to}</div>
-                                                            <div className={"col-2"}>{ethers.utils.formatEther(item.args?.value)}</div>
-                                                        </div>
-                                                    ))
-                                                }
-                                            </> : <div className={"text-info"}>No Transfers from {connectedWallet.address}</div>
-                                        }
-                                    </div>
-                                </div>
-                            </div>
-                        </div>}
-                    </>
-                    }
+                    {connectedWallet.address && connectedWallet.chain?.id == CHAIN_GOERLI.id && <ERC20Container connectedWallet={connectedWallet}/>}
                 </>
                 }
 
             </div>
         </div>
     </div>
+}
+
+
+type ERC20ContainerProps = {
+    connectedWallet: WalletModel
+}
+const ERC20Container: FC<ERC20ContainerProps> = ({connectedWallet}) => {
+    const [loadingErc20data, setLoadingErc20data] = useState<boolean>(true)
+    const [erc20, setErc20] = useState<ERC20Model | null>(null)
+
+    const columnsTransferFrom: TableColumn<Event>[] = [
+        {name: '#', selector: row => row.transactionHash},
+        {name: 'Block', selector: row => row.blockNumber, maxWidth: '50px'},
+        {name: 'Transaction index', selector: row => row.transactionIndex, maxWidth: '150px'},
+        {name: 'To', selector: row => row.args?.to},
+        {name: 'Value in Wei', selector: row => row.args?.value.toString()}
+    ];
+
+    const columnsTransferTo: TableColumn<Event>[] = [
+        {name: '#', selector: row => row.transactionHash},
+        {name: 'Block', selector: row => row.blockNumber, maxWidth: '50px'},
+        {name: 'Transaction index', selector: row => row.transactionIndex, maxWidth: '150px'},
+        {name: 'From', selector: row => row.args?.from},
+        {name: 'Value in Wei', selector: row => row.args?.value.toString()}
+    ];
+
+    useEffect(() => {
+        getERC20Data(connectedWallet.address || '')
+    }, [])
+
+    // Name, symbol, totalSupply, balanceOf signer, tokens transfers from signer, tokens transfers to signer (https://docs.ethers.org/v5/api/contract/example/#erc20-queryfilter)
+    const getERC20Data = (signerAddress: string) => {
+        setLoadingErc20data(true)
+
+        // @ts-ignore
+        const contract = new ethers.Contract(process.env.REACT_APP_GOERLI_ERC20_TOKEN_ADDRESS, [
+            "function name() view returns (string)",
+            "function symbol() view returns (string)",
+            "function totalSupply() view returns (uint256)",
+            "function balanceOf(address) view returns (uint)",
+            "event Transfer(address indexed from, address indexed to, uint value)"
+        ], rpcProvider)
+
+
+        // Name
+        contract.name().then((name: string) => {
+
+            setErc20(prev => ({...prev, name}))
+            // Symbol
+            contract.symbol().then((symbol: string) => {
+                setErc20(prev => ({...prev, symbol: symbol}))
+                // Total supplu
+                contract.totalSupply().then((supply: any) => {
+                    setErc20(prev => ({...prev, totalSupply: ethers.utils.formatEther(supply)}))
+
+                    // Balance of signer
+                    contract.balanceOf(signerAddress).then((balance: any) => {
+
+                        setErc20(prev => ({...prev, balanceOfConnectedAccount: ethers.utils.formatEther(balance)}))
+
+                        // Transfers from signer
+                        const filterTransferFrom = contract.filters.Transfer(signerAddress, null) // Tokens sent from signer
+                        const filterTransferTo = contract.filters.Transfer(null, signerAddress) // Tokens sent to signer
+                        contract.queryFilter(filterTransferFrom).then((r: Event[]) => {
+
+                            setErc20(prev => ({...prev, events: {...prev?.events, transferFrom: r}}))
+                            contract.queryFilter(filterTransferTo).then((r: Event[]) => {
+
+                                console.log(r)
+                                setErc20(prev => ({...prev, events: {...prev?.events, transferTo: r}}))
+
+                                // Listen to incoming events from signer:
+                                contract.on(filterTransferFrom, (from, to, amount, event) => {
+                                    setErc20(prev => ({...prev, events: {...prev?.events, transferFrom: prev?.events?.transferFrom ? [...prev.events.transferFrom, event] : []}}))
+                                });
+                                // Listen to incoming events to signer:
+                                contract.on(filterTransferTo, (from, to, amount, event) => {
+                                    setErc20(prev => ({...prev, events: {...prev?.events, transferTo: prev?.events?.transferTo ? [...prev.events.transferTo, event] : []}}))
+                                });
+
+                                setLoadingErc20data(false)
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    }
+
+    const handleAddGoerliErc20 = async () => {
+        await window.ethereum.request({
+            method: 'wallet_watchAsset',
+            params: {
+                type: 'ERC20',
+                options: {
+                    address: process.env.REACT_APP_GOERLI_ERC20_TOKEN_ADDRESS,
+                    symbol: process.env.REACT_APP_GOERLI_ERC20_TOKEN_SYMBOL,
+                    decimals: 18,
+                    image: ''
+                }
+            }
+        })
+    }
+
+    return <>
+        {loadingErc20data && <span>Loading ERC20 data & events...</span>}
+
+        {!loadingErc20data && erc20 && <div className={"container mt-3"}>
+            <div className="card">
+                <div className="card-header">
+                    <div className="text-center">
+                        ERC20 {erc20.name}
+                    </div>
+
+                </div>
+                <div className="card-body">
+                    <div className="text-center mb-4">
+                        <button className={"btn btn-success"}
+                                onClick={() => handleAddGoerliErc20()}>
+                            Add {erc20.name} to your Wallet
+                        </button>
+                    </div>
+                    <ul>
+                        <li>What's the name of the token? {erc20.name}</li>
+                        <li>How many {erc20.symbol} are they circulating (total supply)? {erc20.totalSupply}</li>
+                        <li>How many {erc20.symbol} tokens does the connected account have? {erc20.balanceOfConnectedAccount}</li>
+                    </ul>
+
+                    {/*Transfer from events*/}
+                    <div className={"pt-2"}>
+                        <h5>Transfers From signer</h5>
+                        {erc20.events!.transferFrom!.length > 0 ?
+                            <DataTable columns={columnsTransferFrom} data={erc20.events!.transferFrom || []} pagination/> :
+                            <div className={"text-info"}>No Transfers from {connectedWallet.address}</div>
+                        }
+                    </div>
+
+
+                    {/*Transfer to events*/}
+                    <div className={"pt-3"}>
+                        <h5>Transfers To Signer</h5>
+                        {erc20.events!.transferTo!.length > 0 ?
+                            <DataTable columns={columnsTransferTo} data={erc20.events!.transferTo || []} pagination/> :
+                            <div className={"text-info"}>No Transfers from {connectedWallet.address}</div>
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>}
+    </>
 }
 
 
